4e0a12b762bf72a0b82eaf4ebffd6ed141b4790e
Aaron D Borden <adborden@a14n.net>
2020-06-27 15:30:04 -0700

    Install packages from crossdev sysroot

--- bin/make-rootfs.sh
+++ bin/make-rootfs.sh
@@ -19,11 +19,32 @@ trap cleanup EXIT
 # Start with the stage3 base
 tar xpf $stage3 --numeric-owner --acls --xattrs -C $rootfs
 
-# Configure portage for arm
-cat <<EOF >> $rootfs/etc/portage/make.conf
-ACCEPT_KEYWORDS="~arm"
-MAKEOPTS="-j4"
+# Install packages from sysroot
+armv7a-unknown-linux-gnueabihf-emerge --sysroot=$rootfs --root=$rootfs -quN --usepkgonly @system
+armv7a-unknown-linux-gnueabihf-emerge --sysroot=$rootfs --root=$rootfs -quN --oneshot --nodeps --usepkgonly net-libs/glib-networking || true  # TODO glib-networking attempts to update gio modules and fails :(
+armv7a-unknown-linux-gnueabihf-emerge --sysroot=$rootfs --root=$rootfs -quN --usepkgonly @asus-c201
+
+# Add system groups
+cat <<EOF >> $rootfs/etc/group
+polkitd:x:240:
+messagebus:x:238:
 EOF
+
+# Add system users https://bugs.gentoo.org/541406
+cat <<EOF >> $rootfs/etc/passwd
+polkitd:x:108:240:System user; polkitd:/var/lib/polkit-1:/sbin/nologin
+messagebus:x:110:238:System user; messagebus:/dev/null:/sbin/nologin
+EOF
+
+# Add password information for shadow
+cat <<EOF >> $rootfs/etc/shadow
+polkitd:*:18393::::::
+messagebus:*:18393::::::
+EOF
+
+
+# Add gentoo user
+useradd --root $rootfs --create-home gentoo
 
 # Set the root password as `gentoo`.
 sed -i 's/root\:\*/root\:\$6\$I9Q9AyTL\$Z76H7wD8mT9JAyrp\/vaYyFwyA5wRVN0tze8pvM\.MqScC7BBm2PU7pLL0h5nSxueqUpYAlZTox4Ag2Dp5vchjJ0/' $rootfs/etc/shadow
@@ -31,4 +52,3 @@ sed -i 's/root\:\*/root\:\$6\$I9Q9AyTL\$Z76H7wD8mT9JAyrp\/vaYyFwyA5wRVN0tze8pvM\
 
 # tar it up
 tar cpJf $workdir/rootfs.tar.xz --numeric-owner --acls --xattrs -C $rootfs .
-rm -rf $rootfs
